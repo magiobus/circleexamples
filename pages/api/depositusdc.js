07263eeef0680fdebf7510df26429dac17360ce4
magiobus <aalbertobus@gmail.com>
2023-06-23 00:54:26 -0600

    Making a failed payment

--- pages/api/depositusdc.js
+++ pages/api/depositusdc.js
@@ -21,6 +21,14 @@ export default async function  handler(req, res) {
   const masterWalletId = configResp.data.data.payments.masterWalletId;
 
 
+  const idempotencyKey = uuidv4()
+  const encryptedData = "LS0tLS1CRUdJTiBQR1AgTUVTU0FHRS0tLS0tCgp3Y0JNQTBYV1NGbEZScFZoQVFmL2J2bVVkNG5LZ3dkbExKVTlEdEFEK0p5c0VOTUxuOUlRUWVGWnZJUWEKMGgzQklpRFNRU0RMZmI0NEs2SXZMeTZRbm54bmFLcWx0MjNUSmtPd2hGWFIrdnNSMU5IbnVHN0lUNWJECmZzeVdleXlNK1JLNUVHV0thZ3NmQ2tWamh2NGloY29xUnlTTGtJbWVmRzVaR0tMRkJTTTBsTFNPWFRURQpiMy91eU1zMVJNb3ZiclNvbXkxa3BybzUveWxabWVtV2ZsU1pWQlhNcTc1dGc1YjVSRVIraXM5ckc0cS8KMXl0M0FOYXA3UDhKekFhZVlyTnVNZGhGZFhvK0NFMC9CQnN3L0NIZXdhTDk4SmRVUEV0NjA5WFRHTG9kCjZtamY0YUtMQ01xd0RFMkNVb3dPdE8vMzVIMitnVDZKS3FoMmtjQUQyaXFlb3luNWcralRHaFNyd3NKWgpIdEphQWVZZXpGQUVOaFo3Q01IOGNsdnhZVWNORnJuNXlMRXVGTkwwZkczZy95S3loclhxQ0o3UFo5b3UKMFVxQjkzQURKWDlJZjRBeVQ2bU9MZm9wUytpT2lLall4bG1NLzhlVWc3OGp1OVJ5T1BXelhyTzdLWTNHClFSWm8KPXc1dEYKLS0tLS1CRUdJTiBQR1AgTUVTU0FHRS0tLS0tCg=="
+  const metadata = {
+    email: 'satoshi@circle.com',
+    sessionId: 'DE6FA86F60BB47B379307F851E238617',
+    ipAddress: '244.28.239.130'
+  }
+
   //Create a Card and get ID
   const newCard = await circle.cards.createCard(
   {
@@ -32,14 +40,10 @@ export default async function  handler(req, res) {
     postalCode: '01234',
     district: 'MA',
   },
-  metadata: {
-    email: 'satoshi@circle.com',
-    sessionId: 'DE6FA86F60BB47B379307F851E238617',
-    ipAddress: '244.28.239.130'
-  },
-  idempotencyKey: 'ba943ff1-ca16-49b2-ba55-1057e70ca5c7',
+  metadata: metadata,
+  idempotencyKey: idempotencyKey,
   keyId: 'key1',
-  encryptedData: 'LS0tLS1CRUdJTiBQR1AgTUVTU0FHRS0tLS0tCgp3Y0JNQTBYV1NGbEZScFZoQVFmL2J2bVVkNG5LZ3dkbExKVTlEdEFEK0p5c0VOTUxuOUlRUWVGWnZJUWEKMGgzQklpRFNRU0RMZmI0NEs2SXZMeTZRbm54bmFLcWx0MjNUSmtPd2hGWFIrdnNSMU5IbnVHN0lUNWJECmZzeVdleXlNK1JLNUVHV0thZ3NmQ2tWamh2NGloY29xUnlTTGtJbWVmRzVaR0tMRkJTTTBsTFNPWFRURQpiMy91eU1zMVJNb3ZiclNvbXkxa3BybzUveWxabWVtV2ZsU1pWQlhNcTc1dGc1YjVSRVIraXM5ckc0cS8KMXl0M0FOYXA3UDhKekFhZVlyTnVNZGhGZFhvK0NFMC9CQnN3L0NIZXdhTDk4SmRVUEV0NjA5WFRHTG9kCjZtamY0YUtMQ01xd0RFMkNVb3dPdE8vMzVIMitnVDZKS3FoMmtjQUQyaXFlb3luNWcralRHaFNyd3NKWgpIdEphQWVZZXpGQUVOaFo3Q01IOGNsdnhZVWNORnJuNXlMRXVGTkwwZkczZy95S3loclhxQ0o3UFo5b3UKMFVxQjkzQURKWDlJZjRBeVQ2bU9MZm9wUytpT2lLall4bG1NLzhlVWc3OGp1OVJ5T1BXelhyTzdLWTNHClFSWm8KPXc1dEYKLS0tLS1CRUdJTiBQR1AgTUVTU0FHRS0tLS0tCg==',
+  encryptedData: encryptedData,
   expMonth: 1,
   expYear: 2020
  }
@@ -49,8 +53,17 @@ export default async function  handler(req, res) {
   const cardId = newCard.data.data.id
   console.log("newCard ID", cardId)
 
-    
-  
+
+  const payment = await circle.payments.createPayment({
+    metadata: metadata,
+    amount: {currency: 'USD', amount: '10'},
+    autoCapture: true,
+    verification: 'cvv',
+    source: {id: cardId, type: 'card'},
+    idempotencyKey: idempotencyKey,
+  } );
+
+  console.log('Payment created:', payment);  
   res.status(200).json(masterWalletId)
  } catch (error) {
     console.log("error", error)
